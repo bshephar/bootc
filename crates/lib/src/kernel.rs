eea1bf2cb015e2aa5378e69e70d0e2c1e1e30437
Brendan Shephard <bshephar@bne-home.net>
2025-07-22 21:37:49 +1000

    Handle non-utf8 chars in /proc/cmdline
    If a user passes some non-utf8 chars via Kernel args, we should
    ensure that we're able to safely handle this without erroring. This
    change uses String::from_utf8_lossy to replace non-utf8 chars while
    parsing the file contents.
    
    Ref:
    https://doc.rust-lang.org/std/string/struct.String.html#method.from_utf8_lossy
    
    Signed-off-by: Brendan Shephard <bshephar@bne-home.net>

--- crates/lib/src/kernel.rs
+++ crates/lib/src/kernel.rs
@@ -13,7 +13,8 @@ pub(crate) const ROOTFLAGS: &str = "rootflags=";
 /// See systemd's code for one userspace parser.
 #[context("Reading /proc/cmdline")]
 pub(crate) fn parse_cmdline() -> Result<Vec<String>> {
-    let cmdline = std::fs::read_to_string("/proc/cmdline")?;
+    let cmdline_bytes = std::fs::read("/proc/cmdline")?;
+    let cmdline = String::from_utf8_lossy(&cmdline_bytes);
     let r = cmdline
         .split_ascii_whitespace()
         .map(ToOwned::to_owned)
@@ -47,5 +48,23 @@ mod tests {
         let kargs = || kargs.iter().copied();
         assert_eq!(find_first_cmdline_arg(kargs(), "root"), Some("/dev/vda"));
         assert_eq!(find_first_cmdline_arg(kargs(), "nonexistent"), None);
+    }
+
+    #[test]
+    fn test_non_utf8_chars() {
+        let raw_input = b"foo=\xFFbar root=/dev/vda blah".to_vec();
+        let result_args = {
+            let cmdline = String::from_utf8_lossy(&raw_input);
+            cmdline
+                .split_ascii_whitespace()
+                .map(ToOwned::to_owned)
+                .collect::<Vec<String>>()
+        };
+        assert_eq!(result_args[0], "foo=\u{FFFD}bar");
+        assert_eq!(result_args[1], "root=/dev/vda");
+        assert_eq!(
+            find_first_cmdline_arg(result_args.iter().map(|s| s.as_str()), "foo"),
+            Some("\u{FFFD}bar")
+        );
     }
 }
